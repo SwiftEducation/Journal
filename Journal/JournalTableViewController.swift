e46abd7fa49e5ae5d9477d18928e9db9e910c26f
Steve Hayman <steve@hayman.net>
2015-12-01 21:27:32 -0500

    Update to Xcode 7.1.1 and Swift 2. References issue #1

--- Journal/JournalTableViewController.swift
+++ Journal/JournalTableViewController.swift
@@ -38,7 +38,7 @@ class JournalTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier, forIndexPath: indexPath) 
         // Configure the cell...
         return cell
     }
