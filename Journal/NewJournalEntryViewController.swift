0f91c205a20f2d2c87b95c7d0d95dbc76286071b
Gabe McFarlane <gabrielmcfarlane65@gmail.com>
2017-04-05 15:08:58 -0700

    Update for Xcode 8 and Swift 3. References #5
    Signed-off-by: Gabe McFarlane <gabrielmcfarlane65@gmail.com>
    Reviewed-by: Yong Bakos <ybakos@humanoriented.com>

--- Journal/NewJournalEntryViewController.swift
+++ Journal/NewJournalEntryViewController.swift
@@ -12,14 +12,14 @@ class NewJournalEntryViewController: UIViewController {
     @IBOutlet weak var journalEntryContents: UITextView!
     var journal: Journal?
     
-    @IBAction func cancel(sender: UIBarButtonItem) {
-        dismissViewControllerAnimated(true, completion: nil)
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func save(sender: UIBarButtonItem) {
-        let entry = JournalEntry(date: NSDate(), contents: journalEntryContents.text)
+    @IBAction func save(_ sender: UIBarButtonItem) {
+        let entry = JournalEntry(date: Date(), contents: journalEntryContents.text)
         journal?.addEntry(entry)
-        dismissViewControllerAnimated(true, completion: nil)
+        dismiss(animated: true, completion: nil)
     }
     
 }
